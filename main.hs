e8c2172439f80eb9e9c513c0641022fa2d7fc72e
Conner D <connerdunn7399@gmail.com>
2016-04-13 09:57:07 -0600

    Will repeat until q or Q

--- main.hs
+++ main.hs
@@ -1,9 +1,22 @@
 import Data.Text (pack, unpack, splitOn)
+import System.IO
 
 main :: IO ()
 main = do
     raw <- readFile "/Users/Conner/Desktop/282Questions.txt"
-    print $ parse raw
+    hSetBuffering stdin NoBuffering
+    doUntilQ raw
+
+doUntilQ :: String -> IO ()
+doUntilQ raw = do
+    x <- getLine
+    case x of
+         "q" -> return ()
+         "Q" -> return ()
+         _   ->  do
+             print ("The first question is" : [x])
+             print $ parse raw
+             doUntilQ raw
 
 parse :: String -> [[String]]
 parse s = map splitToTuple $ lines s
