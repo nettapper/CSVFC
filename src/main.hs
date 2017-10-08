e04dd9f02dd6de96a2e134878245c5373f382abe
Conner D <connerdunn7399@gmail.com>
2017-10-08 11:41:14 -0600

    The great refactor

--- src/main.hs
+++ src/main.hs
@@ -1,38 +1,53 @@
 module Main where
 
 import Data.Char (toLower)
-import Data.Text (pack, unpack, splitOn)
+import Data.Text (pack, unpack, breakOn, Text, tail, strip)
 import System.IO
+import Control.Monad (forever)
 import System.Random (randomRIO)
 
+data Card = Card
+  { front :: Text
+  , back :: Text
+  }
+
+filePath :: String
+filePath = "./test.csv"
+
 main = do
-    raw <- readFile "/Users/Conner/Desktop/282Questions.txt"
     hSetBuffering stdin NoBuffering
-    doUntilQ $ parse raw
+    putStrLn $ "Reading in from file: " ++ filePath
+    raw <- readFile filePath
+    putStrLn "Press Ctrl-C / Ctrl-D to exit, depending on your system."
+    putStrLn ""
+    doUntilQ $ parseFileContents raw
 
-doUntilQ :: [[String]] -> IO ()
-doUntilQ cards = do
-    x <- getChar
-    case (toLower x) of
-         'q' -> return ()
-         _   ->  do
-             putStrLn "\nThe question is..."
-             randCard <- pick cards
-             printCard randCard
-             doUntilQ cards
+doUntilQ :: [Card] -> IO ()
+doUntilQ cards = forever $ do
+  randCard <- pickRandOne cards
+  interactiveShow randCard
+  doUntilQ cards
 
-pick :: [a] -> IO a
-pick xs = fmap (xs !!) (randomRIO (0, length xs - 1))
+pickRandOne :: [a] -> IO a
+pickRandOne xs = fmap (xs !!) (randomRIO (0, length xs - 1))
 
-printCard :: [String] -> IO ()
-printCard card = do
-    print $  head card
-    x <- getChar
-    print $ head $ tail card
+interactiveShow :: Card -> IO ()
+interactiveShow card = do
+  putStrLn "Press any key when ready to see the answer and when ready to proceed..."
+  putStrLn $ "Q: " ++ (unpack $ front card)
+  x <- getChar
+  putStr "\b"  -- a sneaky way to delete the getChar from above
+  putStrLn $ "A: " ++ (unpack $ back card)
+  putStrLn ""
+  x <- getChar
+  putStr "\b"  -- a sneaky way to delete the getChar from above
+  return ()
 
+parseFileContents :: String -> [Card]
+parseFileContents s = map splitToCard $ lines s
 
-parse :: String -> [[String]]
-parse s = map splitToTuple $ lines s
-
-splitToTuple :: String -> [String]
-splitToTuple s = map unpack $ splitOn (pack ",") $ pack s
+splitToCard :: String -> Card
+splitToCard s = Card {front = f, back = b}
+  where cardTuple = breakOn (pack ",") $ pack s
+        f = fst cardTuple
+        b = Data.Text.strip $ Data.Text.tail $ snd cardTuple
