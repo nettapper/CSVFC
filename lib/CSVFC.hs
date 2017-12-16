908fb6ad41f658d039bbe0cf5f729b49bcb0c9a4
Conner D <connerdunn7399@gmail.com>
2017-12-16 15:04:08 -0700

    And refactoring was pretty easy!

--- lib/CSVFC.hs
+++ lib/CSVFC.hs
@@ -27,10 +27,7 @@ maybeExtract i as =
 
 maybeInsertAtK :: Int -> Maybe a -> [a] -> [a]
 maybeInsertAtK _ Nothing xs = xs
-maybeInsertAtK index (Just a) xs
-  | index <= 0 = a : xs
-  | index >= (length xs) = reverse $ a : (reverse xs)
-  | otherwise = (take index xs) ++ [a] ++ (drop index xs)
+maybeInsertAtK index (Just a) xs = (take index xs) ++ [a] ++ (drop index xs)
 
 parseFileContents :: String -> [Card]
 parseFileContents s = map splitToCard $ filter (not . isEmpty) $ map process $ lines s
